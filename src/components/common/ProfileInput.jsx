b3e0e7cddcfcf962af8956c800235f41280b9dd3
juhoon212 <ws5501@naver.com>
2024-06-05 14:57:27 +0900

    profile update

--- src/components/common/ProfileInput.jsx
+++ src/components/common/ProfileInput.jsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { joinProfileImgAtom } from '../../recoil/atoms/LoginAtoms';
+import userImg from '../../../public/user.png';
 function ProfileInput({children, ...props}) {
 
     const [image, setImage] = useRecoilState(joinProfileImgAtom);
@@ -9,7 +9,7 @@ function ProfileInput({children, ...props}) {
     return (
         <>
             <label htmlFor={props.id} className={props.css}>
-                <img src={image.addUserImg} 
+                <img src={userImg} 
                 alt='프로필'
                 className='w-28 inline border-2 border-white rounded-full hover:scale-110 transition cursor-pointer hover:border-blue-500 placeholder:text-white'
                 />
