114851bdd50f59f5876b6448ec77b3e3e7c69548
juhoon212 <ws5501@naver.com>
2024-06-26 18:16:10 +0900

    sub login-policy

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,27 @@
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import MainNav from "../components/header/MainNav"
 import axios from "axios"
 import { useEffect } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { TokenAtom } from "../recoil/atoms/LoginAtoms";
 
 function MainLayout() {
 
     const navigate = useNavigate();
-    const token = useRecoilValue(TokenAtom);
+    const [token, setToken] = useRecoilState(TokenAtom);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
 
     useEffect(() => {
-        // 임시 => 추후 홈 페이지에 불러올 데이터가 있을 경우 헤더에 토큰을 넣어서 서버로 보낸 후 유효기간 검증
-        if (token == '') {
-            navigate("/login");
+        console.log(token);
+        const queryStringToken = searchParams.get('token');
+        if (queryStringToken != null) {
+            console.log(queryStringToken);
+            setToken(queryStringToken);
+        } else {
+            if (token == '' || token == null || token == undefined) {
+                navigate('/login');
+            }
         }
     },[])
 
